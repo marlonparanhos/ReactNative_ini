dd2a4a002a0818261b2dbe74277ec2be46d396b0
Marlon Paranhos <marlonparanhos144@gmail.com>
2019-02-25 01:04:38 -0300

    + components

--- mines/App.js
+++ mines/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
+import Fields from './src/components/Fields'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text styles={styles.welcome}>Iniciando o Mines!</Text>
-        <Text styles={styles.instructions}>Tamanho da grade:
+        <Text style={styles.welcome}>Iniciando o Mines!</Text>
+        <Text style={styles.instructions}>Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        
+        <Fields />
+        <Fields opened />
+        <Fields opened nearMines={1} />
+        <Fields opened nearMines={2} />
+        <Fields opened nearMines={3} />
+        <Fields opened nearMines={6} />
+        <Fields mined />
+        <Fields opened mined />
+        <Fields opened mined exploded />
+        <Fields flagged />
+        <Fields opened flagged />
       </View>
     );
   }
