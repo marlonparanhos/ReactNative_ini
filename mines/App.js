0d1f943c18b6ae81e2b93a3e25f1f5faf141bc9b
Marlon Paranhos <marlonparanhos144@gmail.com>
2019-02-27 01:21:32 -0300

    Complete

--- mines/App.js
+++ mines/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import params from './src/params'
 import MineField from './src/components/MineField'
-import { createMinedBoard } from './src/functions'
+import Header from './src/components/Header'
+import LevelSelection from './src/screens/LevelSelection'
+import {
+  createMinedBoard,
+  cloneBoard,
+  openField,
+  hadExplosion,
+  wonGame,
+  showMines,
+  invertFlag,
+  flagsUsed
+} from './src/functions'
 
 export default class App extends Component {
 
@@ -22,20 +33,59 @@ export default class App extends Component {
     const rows = params.getRowsAmount()
     return {
       board : createMinedBoard(rows, cols, this.minesAmount()),
+      won : false,
+      lost : false,
+      showLevelSelection: false,
     }
   }
 
+  onOpenField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    openField(board, row, column)
+    const lost = hadExplosion(board)
+    const won = wonGame(board)
+
+    if (lost) {
+      showMines(board)
+      Alert.alert('Perdeeeeeeu!', 'Que buuuuuurro!')
+    }
+
+    if (won) {
+      Alert.alert('Parabéns!', 'Você Venceu!')
+    }
+
+    this.setState({ board, lost, won })
+  }
+
+  onSelectField = (row, column) => {
+    const board = cloneBoard(this.state.board)
+    invertFlag(board, row, column)
+    const won = wonGame(board)
+
+    if (won) {
+      Alert.alert('Parabéns!', 'Você Venceu!')
+    }
+
+    this.setState({ board, won })
+  }
+  
+  onLevelSelected = level => {
+    params.difficultLevel = level
+    this.setState(this.createState())
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Iniciando o Mines!</Text>
-        <Text style={styles.instructions}>Tamanho da grade:
-          {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      <LevelSelection isVisible={this.state.showLevelSelection} onLevelSelected={this.onLevelSelected}
+      onCancel={() => this.setState({showLevelSelection: false})}></LevelSelection>
 
-          <View style={styles.board}>
-            <MineField board={this.state.board} />
-          </View>
+        <Header flagsLeft={this.minesAmount() - flagsUsed(this.state.board)} onNewGame={() => this.setState(this.createState())}
+          onFlagPress={() => this.setState({ showLevelSelection: true })}/>
+
+        <View style={styles.board}>
+          <MineField board={this.state.board} onOpenField={this.onOpenField} onSelectField={this.onSelectField} />
+        </View>
       </View>
     );
   }
--- mines/src/components/MineField.js
+++ mines/src/components/MineField.js
@@ -6,7 +6,9 @@ export default props => {
     const rows = props.board.map((row, r) => {
         const columns = row.map((field, c) => { // armazena todos os fields de uma linha
             // transforma um array de objetos em um array de elementos jsx
-            return <Field {...field} key={c} /> // a ausência do key não gera erro, mas é um boa prática usar (é tipo ID)
+            return <Field {...field} key={c} // a ausência do key não gera erro, mas é um boa prática usar (é tipo ID)
+                onOpen={() => props.onOpenField(r, c)} // abre mina
+                onSelect={() =>props.onSelectField(r, c)}/> // sinaliza a mina (flag)
         })
         
         return <View key={r} style={{flexDirection: 'row'}}>{columns}</View>
